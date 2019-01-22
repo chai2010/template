e4f8b9810d0849adc3883b00db083b33bd0ed411
chai2010 <chaishushan@gmail.com>
2019-01-23 02:27:58 +0800

    zz

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package template provides some template helper functions.
+package template
